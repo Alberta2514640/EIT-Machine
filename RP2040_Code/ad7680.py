2b46e063ed81acf48317c76c51880ab240569adc
Jackie Wu <77469927+JW-tldgn@users.noreply.github.com>
2024-04-02 01:42:59 -0600

    Fixed AD7680 bugs

--- RP2040_Code/ad7680.py
+++ RP2040_Code/ad7680.py
@@ -10,12 +10,15 @@ import user_spi
 # Returns the formatted 16-bit value that is received by the ADC. The host will convert it to a floating-point number
 # since the RP2040 doesn't have a dedicated FPU.
 # Hopefully, this could mean that we can have really fast reconstructions, approaching real-time.
-def single_conv (spi:busio.SPI, cs:digitalio.DigitalInOut, outbuffer:bytearray):
-    if user_spi.user_try_lock(spi, cs):
-        outbuffer = bytearray(3)
-        user_spi.read(spi, cs, 3, outbuffer)
-    else:
-        print ("Failed to read from SPI)")
-        return
-    outval = ((outbuffer[0] & 0x0F)) << 12 | (outbuffer[1] << 4) | ((outbuffer[2] & 0xF0) >> 4)
+def single_conv (spi:busio.SPI, cs:digitalio.DigitalInOut):
+    spi.try_lock()
+    cs.value = False
+    buffer = bytearray(3)
+    spi.readinto(buffer)
+    # print (buffer)
+    cs.value = True
+    spi.unlock()
+    # print (buffer[2])
+    outval = (((buffer[0] & 0x0F)) << 12) | ((buffer[1] << 4)) | (((buffer[2] & 0xF0) >> 4))
+    # print (outval)
     return outval
